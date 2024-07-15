b21e6fcb24c1a5c1efcfec327627326cc268a191
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2024-07-15 15:59:30 +0200

    Added more things to c++ example

--- hardware-emulator/cpp-example/main.cpp
+++ hardware-emulator/cpp-example/main.cpp
@@ -6,13 +6,19 @@ int main() {
     using VoidType = void* (__cdecl*)();
     using GCType = void* (__cdecl*)(int&, int&);
     using MTType = void* (__cdecl*)(int, int);
-    using keyType = void* (__cdecl*)(const char*);
+    using KeyType = void* (__cdecl*)(const char*, int ms);
+    using CType = void* (__cdecl*)(const char*);
+    using DCType = void* (__cdecl*)(const char*, const char*);
 
     VoidType const showInputDevices = reinterpret_cast<VoidType>(GetProcAddress(HEDll, "showInputDevices"));
     GCType const getCursorPos = reinterpret_cast<GCType>(GetProcAddress(HEDll, "getCursorPos"));
     MTType const mouseTP = reinterpret_cast<MTType>(GetProcAddress(HEDll, "mouseTP"));
-    keyType const key = reinterpret_cast<keyType>(GetProcAddress(HEDll, "key"));
+    KeyType const key = reinterpret_cast<KeyType>(GetProcAddress(HEDll, "key"));
     MTType const mouseMove = reinterpret_cast<MTType>(GetProcAddress(HEDll, "mouseMove"));
+    CType const manageCD = reinterpret_cast<CType>(GetProcAddress(HEDll, "manageCD"));
+    DCType const playAudio = reinterpret_cast<DCType>(GetProcAddress(HEDll, "playAudio"));
+    CType const stopAudio = reinterpret_cast<CType>(GetProcAddress(HEDll, "stopAudio"));
+    CType const beep = reinterpret_cast<CType>(GetProcAddress(HEDll, "beep"));
 
     Sleep(5000);
     showInputDevices();
@@ -20,6 +26,13 @@ int main() {
     getCursorPos(x, y);
     std::cout << "x: " <<  std::to_string(x) << "\ny: " << std::to_string(y) << "\n";
     mouseTP(10, 10);
+    manageCD("open");
+    Sleep(1000);
+    manageCD("close");
+    playAudio("../python-example/rick.mp3", "rickroll");
+    Sleep(5000);
+    stopAudio("rickroll");
+    beep("click");
 
     FreeLibrary(HEDll);
     return 0;
