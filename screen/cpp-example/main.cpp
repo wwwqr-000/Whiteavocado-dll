79c34b56ddf51051163df2c3f97dd9fdd461e233
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2024-07-18 17:57:28 +0200

    Added takeScreenshot method to dll and cpp-example

--- screen/cpp-example/main.cpp
+++ screen/cpp-example/main.cpp
@@ -9,10 +9,12 @@ int main() {
     using GP = void (__cdecl*)(int, int, int&, int&, int&);
     using GCP = void (__cdecl*)(int&, int&);
     using DI = int (__cdecl*)(const char*, int, int, int, int);
+    using TS = bool (__cdecl*)(const char*);
 
     GP const getPixelValue = reinterpret_cast<GP>(GetProcAddress(SDll, "getPixelValue"));
     GCP const getCursorPos = reinterpret_cast<GCP>(GetProcAddress(SDll, "getCursorPos"));
     DI const drawBMP = reinterpret_cast<DI>(GetProcAddress(SDll, "drawBMP"));
+    TS const takeScreenshot = reinterpret_cast<TS>(GetProcAddress(SDll, "takeScreenshot"));
 
     Sleep(2000);
 
@@ -27,6 +29,8 @@ int main() {
     for (int i, off; i <= 20; i++, off += 40) {//Example with test.bmp format (Draw 20 times)
         std::cout << drawBMP("test.bmp", off, 10, off, 50) << "\n";
     }
+
+    std::cout << takeScreenshot("screenshot.bmp") << "\n";//Take a screenshot
 
     return 0;
 }
