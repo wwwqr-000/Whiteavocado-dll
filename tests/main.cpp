5dc6cf0c85c6d012ef57df795497b5a348fd7445
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2024-07-24 01:01:12 +0200

    Updated test case

--- tests/main.cpp
+++ tests/main.cpp
@@ -6,6 +6,7 @@ HMODULE WDll = LoadLibraryExW(L"../all/bin/Release/whiteavocado64.dll", nullptr,
 using GUN = const char* (__cdecl*)();
 using SF = std::string (__cdecl*)(std::string);
 using AC = bool (__cdecl*)(const char*);
+using TS = bool (__cdecl*)(const char*, int, int, int, int);
 using IS = bool (__cdecl*)();
 using MST = int (__cdecl*)(const char*, const char*);
 using PA = void (__cdecl*)(const char*, const char*);
@@ -20,22 +21,23 @@ using DL = void (__cdecl*)(int, int, int, int, int, int, int, int, bool);
 using DB = int (__cdecl*)(const char*, int, int, int, int);
 using GP = void (__cdecl*)(int, int, int&, int&, int&);
 using MB = void (__cdecl*)(const char*, const char*, const char*, const char*);
+using KL = char (__cdecl*)(const char*, std::string&);
 
-GUN const getUName = reinterpret_cast<GUN>(GetProcAddress(WDll, "getUName"));                  //checking in main
-GUN const getSelfName = reinterpret_cast<GUN>(GetProcAddress(WDll, "getSelfName"));            //checking in main
+GUN const getUName = reinterpret_cast<GUN>(GetProcAddress(WDll, "getUName"));                           //checking in main
+GUN const getSelfName = reinterpret_cast<GUN>(GetProcAddress(WDll, "getSelfName"));                     //checking in main
 SF const selectFolder = reinterpret_cast<SF>(GetProcAddress(WDll, "selectFolder"));
 SF const selectFile = reinterpret_cast<SF>(GetProcAddress(WDll, "selectFile"));
-AC const access = reinterpret_cast<AC>(GetProcAddress(WDll, "access"));                        //checking in main
-IS const isStartup = reinterpret_cast<IS>(GetProcAddress(WDll, "isStartup"));                  //checking in main
+AC const access = reinterpret_cast<AC>(GetProcAddress(WDll, "access"));                                 //checking in main
+IS const isStartup = reinterpret_cast<IS>(GetProcAddress(WDll, "isStartup"));                           //checking in main
 MST const moveSelfStartup = reinterpret_cast<MST>(GetProcAddress(WDll, "moveSelfStartup"));
 PA const playAudio = reinterpret_cast<PA>(GetProcAddress(WDll, "playAudio"));
 SA const stopAudio = reinterpret_cast<SA>(GetProcAddress(WDll, "stopAudio"));
-SD const showInputDevices = reinterpret_cast<SD>(GetProcAddress(WDll, "showInputDevices"));    //checking in main
+SD const showInputDevices = reinterpret_cast<SD>(GetProcAddress(WDll, "showInputDevices"));             //checking in main
 SR const startRecording = reinterpret_cast<SR>(GetProcAddress(WDll, "startRecording"));
 SA const stopRecording = reinterpret_cast<SA>(GetProcAddress(WDll, "stopRecording"));
 PA const saveRecording = reinterpret_cast<PA>(GetProcAddress(WDll, "saveRecording"));
 SA const manageCD = reinterpret_cast<SA>(GetProcAddress(WDll, "manageCD"));
-SA const beep = reinterpret_cast<SA>(GetProcAddress(WDll, "beep"));                            //checking in main
+SA const beep = reinterpret_cast<SA>(GetProcAddress(WDll, "beep"));                                     //checking in main
 KY const key = reinterpret_cast<KY>(GetProcAddress(WDll, "key"));
 MTP const mouseTP = reinterpret_cast<MTP>(GetProcAddress(WDll, "mouseTP"));
 MTP const mouseMove = reinterpret_cast<MTP>(GetProcAddress(WDll, "mouseMove"));
@@ -44,9 +46,11 @@ GCP const getCursorPos = reinterpret_cast<GCP>(GetProcAddress(WDll, "getCursorPo
 DP const drawPixel = reinterpret_cast<DP>(GetProcAddress(WDll, "drawPixel"));
 DL const drawLine = reinterpret_cast<DL>(GetProcAddress(WDll, "drawLine"));
 DB const drawBMP = reinterpret_cast<DB>(GetProcAddress(WDll, "drawBMP"));
-AC const takeScreenshot = reinterpret_cast<AC>(GetProcAddress(WDll, "takeScreenshot"));
+TS const takeScreenshot = reinterpret_cast<TS>(GetProcAddress(WDll, "takeScreenshot"));
 GP const getPixelValue = reinterpret_cast<GP>(GetProcAddress(WDll, "getPixelValue"));
 MB const msgBox = reinterpret_cast<MB>(GetProcAddress(WDll, "msgBox"));
+GCP const getScreenResolution = reinterpret_cast<GCP>(GetProcAddress(WDll, "getScreenResolution"));     //checking in main
+KL const keyListener = reinterpret_cast<KL>(GetProcAddress(WDll, "keyListener"));
 
 int main() {
     std::string username(getUName());
@@ -60,6 +64,9 @@ int main() {
     std::cout << s << "\n";
     showInputDevices();
     beep("click");
+    int resX, resY;
+    getScreenResolution(resX, resY);
+    std::cout << "Screen res: " << resX << "x" << resY << "\n";
 
 
     FreeLibrary(WDll);
